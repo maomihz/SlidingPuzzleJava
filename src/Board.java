21c7621b1ab165d5c822466c5f489ffb7fb3aa1b
Seancheey <adls371@163.com>
2015-07-18 19:27:39 -0700

    organize everything

--- src/Board.java
+++ src/Board.java
@@ -1,18 +1,16 @@
 import java.awt.Point;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class Board {
-	
-	private int[][] board;
-	private Point zeroPos;
-	
 	public static final int DIRECTION_UP = 0;
 	public static final int DIRECTION_RIGHT = 1;
+	
 	public static final int DIRECTION_DOWN = 2;
 	public static final int DIRECTION_LEFT = 3;
+	private int[][] board;
 	
+	
+	private Point zeroPos;
 	
 	public Board() {
 		board = new int[4][4];
@@ -35,51 +33,6 @@ public class Board {
 		
 	}
 	
-	//Shuffle, default 300
-	public void shuffle() {
-		this.shuffle(300);
-	}
-	
-	public void shuffle(int times) {
-		for (int i=0;i<times;i++) {
-			move((int)(Math.random() * 4));
-		}
-	}
-	
-	
-	//Methods for moving
-	public void up() {
-		if (canMove(DIRECTION_UP)) {
-			board[zeroPos.x][zeroPos.y] = board[zeroPos.x + 1][zeroPos.y];
-			board[zeroPos.x + 1][zeroPos.y] = 0;
-			zeroPos.x += 1;
-		}
-	}
-	
-	public void down() {
-		if (canMove(DIRECTION_DOWN)) {
-			board[zeroPos.x][zeroPos.y] = board[zeroPos.x - 1][zeroPos.y];
-			board[zeroPos.x - 1][zeroPos.y] = 0;
-			zeroPos.x -= 1;
-		}
-	}
-	
-	public void left() {
-		if (canMove(DIRECTION_LEFT)) {
-			board[zeroPos.x][zeroPos.y] = board[zeroPos.x][zeroPos.y + 1];
-			board[zeroPos.x][zeroPos.y + 1] = 0;
-			zeroPos.y += 1;
-		}
-	}
-	
-	public void right() {
-		if (canMove(DIRECTION_RIGHT)) {
-			board[zeroPos.x][zeroPos.y] = board[zeroPos.x][zeroPos.y - 1];
-			board[zeroPos.x][zeroPos.y - 1] = 0;
-			zeroPos.y -= 1;
-		}
-	}
-	
 	//Check whether can move
 	public boolean canMove(int direction) {
 		if(direction == DIRECTION_UP && zeroPos.x != 3) {
@@ -95,6 +48,70 @@ public class Board {
 			return true;
 		}
 		return false;
+	}
+	
+	
+	public boolean checkWin() {
+		if (board[3][3] != 0) 
+			return false;
+		for (int i=0;i<4;i++) {
+			for (int j=0;j<4;j++) {
+				if (board[i][j] != i * 4 + j + 1) {
+					if (i != 3 || j != 3)
+						return false;
+					else {
+						return board[i][j] == 0;
+					}
+				}
+			}
+		}
+		return true;
+	}
+	
+	public void down() {
+		if (canMove(DIRECTION_DOWN)) {
+			board[zeroPos.x][zeroPos.y] = board[zeroPos.x - 1][zeroPos.y];
+			board[zeroPos.x - 1][zeroPos.y] = 0;
+			zeroPos.x -= 1;
+		}
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Board other = (Board) obj;
+		if (!Arrays.deepEquals(board, other.board))
+			return false;
+		return true;
+	}
+	
+	public int[][] getBoard() {
+		return board;
+	}
+	
+	public Point getZero() {
+		return zeroPos;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + Arrays.deepHashCode(board);
+		return result;
+	}
+	
+	public void left() {
+		if (canMove(DIRECTION_LEFT)) {
+			board[zeroPos.x][zeroPos.y] = board[zeroPos.x][zeroPos.y + 1];
+			board[zeroPos.x][zeroPos.y + 1] = 0;
+			zeroPos.y += 1;
+		}
 	}
 	
 	//Move in a specified direction
@@ -117,34 +134,31 @@ public class Board {
 		}
 	}
 	
-	public Point getZero() {
-		return zeroPos;
-	}
 	
-	public int[][] getBoard() {
-		return board;
-	}
-	
-	
-	public boolean checkWin() {
-		if (board[3][3] != 0) 
-			return false;
-		for (int i=0;i<4;i++) {
-			for (int j=0;j<4;j++) {
-				if (board[i][j] != i * 4 + j + 1) {
-					if (i != 3 || j != 3)
-						return false;
-					else {
-						return board[i][j] == 0;
-					}
-				}
-			}
+	public void right() {
+		if (canMove(DIRECTION_RIGHT)) {
+			board[zeroPos.x][zeroPos.y] = board[zeroPos.x][zeroPos.y - 1];
+			board[zeroPos.x][zeroPos.y - 1] = 0;
+			zeroPos.y -= 1;
 		}
-		return true;
 	}
 	
 	
 	
+	//Shuffle, default 300
+	public void shuffle() {
+		this.shuffle(300);
+	}
+	
+
+	
+
+	public void shuffle(int times) {
+		for (int i=0;i<times;i++) {
+			move((int)(Math.random() * 4));
+		}
+	}
+
 	public String toString() {
 		String result = "";
 		for (int[] i : board) {
@@ -154,31 +168,21 @@ public class Board {
 		}
 		return result;
 	}
-	
-	
-	public int hashCode() {
-		return toString().hashCode();
-	}
-	
-	
-	public boolean equals(Object obj) {
-		Board ano = (Board)obj;
-		for (int i=0;i<4;i++) {
-			for (int j=0;j<4;j++) {
-				if (board[i][j] != ano.board[i][j])
-					return false;
-			}
-		}
-		return true;
-	}
-	
 
+	//Methods for moving
+	public void up() {
+		if (canMove(DIRECTION_UP)) {
+			board[zeroPos.x][zeroPos.y] = board[zeroPos.x + 1][zeroPos.y];
+			board[zeroPos.x + 1][zeroPos.y] = 0;
+			zeroPos.x += 1;
+		}
+	}
 	public static void main(String[] args) {
 		Board board = new Board();
 		board.shuffle();
 		Gui window = new Gui(board);
 		window.setVisible(true);
-
+		
 	}
 
 }
--- src/Gui.java
+++ src/Gui.java
@@ -1,9 +1,14 @@
-import java.awt.*;
-import javax.swing.*;
-import java.awt.event.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
 
 public class Gui extends JFrame implements KeyListener {
-	
+	private static final long serialVersionUID = 1L;
 	private Board game;
 	private JPanel panel;
 	private JButton[][] buttons;
